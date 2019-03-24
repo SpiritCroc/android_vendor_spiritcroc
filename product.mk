b716877f9159dd419af714702e6d15f3b53d368b
SpiritCroc <dev@spiritcroc.de>
2019-03-24 19:42:22 +0100

    Remove icon mask overlay
    AICP now has a system setting for that

--- product.mk
+++ product.mk
@@ -1,8 +1,5 @@
 VENDOR_EXTRA_PATH := vendor/extra
 
-# Extra overlays
-PRODUCT_PACKAGE_OVERLAYS += $(VENDOR_EXTRA_PATH)/overlay/common
-
 # Custom bootanimation
 PRODUCT_COPY_FILES += \
     $(VENDOR_EXTRA_PATH)/bootanimation/bootanimation.zip:system/media/bootanimation.zip
