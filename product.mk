cd978b9d18695550569a47f783f28f0c2a79da10
SpiritCroc <dev@spiritcroc.de>
2019-12-03 19:28:42 +0100

    Put custom bootanimation into product
    Allows overriding bootanimation without BUILD_BROKEN_DUP_RULES.

--- product.mk
+++ product.mk
@@ -2,7 +2,7 @@ VENDOR_EXTRA_PATH := vendor/extra
 
 # Custom bootanimation
 PRODUCT_COPY_FILES += \
-    $(VENDOR_EXTRA_PATH)/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    $(VENDOR_EXTRA_PATH)/bootanimation/bootanimation.zip:system/product/media/bootanimation.zip
 
 # Extra overlays
 PRODUCT_PACKAGE_OVERLAYS += $(VENDOR_EXTRA_PATH)/overlay/common
