697deedf0203ad28aa020d4f3437d06c9211c6ee
SpiritCroc <dev@spiritcroc.de>
2023-02-01 14:32:34 +0100

    Do not include F-Droid and AuroraStore due to release signature mismatch

--- product.mk
+++ product.mk
@@ -6,10 +6,16 @@ TARGET_BOOTANIMATION := $(VENDOR_EXTRA_PATH)/bootanimation/bootanimation.zip
 # Extra overlays
 PRODUCT_PACKAGE_OVERLAYS += $(VENDOR_EXTRA_PATH)/overlay/common
 
-# Play Store alternatives system integrations from omni prebuilts
+# Play Store alternatives system integrations
 PRODUCT_PACKAGES += \
     AuroraServices \
 	F-DroidPrivilegedExtension
 
 # MicroG from omni prebuilts
-$(call inherit-product-if-exists, vendor/prebuiltapks/microg.mk)
+PRODUCT_PACKAGES += \
+    FakeStore \
+    GmsCore \
+    GsfProxy \
+    microg-a5k.xml \
+    MozillaNlpBackend \
+    NominatimNlpBackend
